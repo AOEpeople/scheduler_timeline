acad895ab3659f3e311c3fc6185b7e58aabbeb42
Stefan Rotsch <stefan.rotsch@aoe.com>
2017-09-01 12:37:46 +0200

    [CLEANUP] Reformat code

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,27 +2,26 @@
 # Table structure for table 'tx_schedulertimeline_domain_model_log'
 #
 CREATE TABLE tx_schedulertimeline_domain_model_log (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-    tstamp int(11) DEFAULT '0' NOT NULL,
-    crdate int(11) DEFAULT '0' NOT NULL,
-    cruser_id int(11) DEFAULT '0' NOT NULL,
+  uid           INT(11)             NOT NULL AUTO_INCREMENT,
+  pid           INT(11) DEFAULT '0' NOT NULL,
+  tstamp        INT(11) DEFAULT '0' NOT NULL,
+  crdate        INT(11) DEFAULT '0' NOT NULL,
+  cruser_id     INT(11) DEFAULT '0' NOT NULL,
 
-    task int(11) DEFAULT '0' NOT NULL,
-    starttime int(11) DEFAULT '0' NOT NULL,
-    endtime int(11) DEFAULT '0' NOT NULL,
-    exception text NOT NULL,
-    returnmessage text NOT NULL,
-    processid int(11) DEFAULT '0' NOT NULL,
+  task          INT(11) DEFAULT '0' NOT NULL,
+  starttime     INT(11) DEFAULT '0' NOT NULL,
+  endtime       INT(11) DEFAULT '0' NOT NULL,
+  exception     TEXT                NOT NULL,
+  returnmessage TEXT                NOT NULL,
+  processid     INT(11) DEFAULT '0' NOT NULL,
 
-    PRIMARY KEY (uid),
-    KEY parent (pid)
+  PRIMARY KEY (uid),
+  KEY parent (pid)
 );
 
 #
 # Table structure for table 'tx_scheduler_task'
 #
 CREATE TABLE tx_scheduler_task (
-    pid int(11) DEFAULT '0' NOT NULL,
+  pid INT(11) DEFAULT '0' NOT NULL
 );
-
