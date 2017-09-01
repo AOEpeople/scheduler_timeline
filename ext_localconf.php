82d9f57da6eff11b8a9a79574514dea2dcfd3a01
Stefan Rotsch <stefan.rotsch@aoe.com>
2017-09-01 12:36:15 +0200

    [CLEANUP] Streamline ext_localconf.php and ext_emconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,12 @@
 <?php
+defined('TYPO3_MODE') or die();
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS']['ext/scheduler/class.tx_scheduler.php'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/XClass/Scheduler.php';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS']['ext/scheduler/class.tx_scheduler.php'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/XClass/Scheduler.php';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_scheduler']['className'] = 'Scheduler';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Scheduler\\Scheduler']['className'] = 'AOE\\SchedulerTimeline\\XClass\\Scheduler';
 
-    // Get the extensions's configuration
+// Get the extensions's configuration
 $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['scheduler_timeline']);
 
 if ((TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_AJAX) || ('BE' === TYPO3_MODE)) {
