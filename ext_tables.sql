c4f4c8f9a2e559acad5bed485209613731d0fe2b
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2018-04-20 09:37:10 +0200

    [TASK] Updated SQL Strcuture to match install-tool requirements

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@
 # Table structure for table 'tx_schedulertimeline_domain_model_log'
 #
 CREATE TABLE tx_schedulertimeline_domain_model_log (
-  uid           INT(11)             NOT NULL AUTO_INCREMENT,
+  uid           INT(11)             NOT NULL auto_increment,
   pid           INT(11) DEFAULT '0' NOT NULL,
   tstamp        INT(11) DEFAULT '0' NOT NULL,
   crdate        INT(11) DEFAULT '0' NOT NULL,
@@ -11,8 +11,8 @@ CREATE TABLE tx_schedulertimeline_domain_model_log (
   task          INT(11) DEFAULT '0' NOT NULL,
   starttime     INT(11) DEFAULT '0' NOT NULL,
   endtime       INT(11) DEFAULT '0' NOT NULL,
-  exception     TEXT                NOT NULL,
-  returnmessage TEXT                NOT NULL,
+  exception     text                NOT NULL,
+  returnmessage text                NOT NULL,
   processid     INT(11) DEFAULT '0' NOT NULL,
 
   PRIMARY KEY (uid),
